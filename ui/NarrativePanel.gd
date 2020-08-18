e0e58ee1dfd1ef80418c25891f554d5d70f86bf5
Jakob Eklund <jakob.eklund@superattack.se>
2020-08-18 11:07:11 +0200

    Split longer conversation segments

--- ui/NarrativePanel.gd
+++ ui/NarrativePanel.gd
@@ -7,6 +7,8 @@ var NarrationBox: PackedScene = preload("res://ui/NarrationBox.tscn")
 
 signal pressentation_completed()
 
+var _postponed_elements:Array = []
+
 func _ready():
 	# Remove placeholders
 	clear()
@@ -15,12 +17,41 @@ func queue_dialouge(speaker: Person, dialoug: String):
 	var d = DialougeBox.instance()
 	d.get_node("speaker_name").text = speaker.speaker_name
 	d.get_node("dialouge_label").bbcode_text = dialoug
-	$margin/vbox.add_child(d)
+	self.queue_element(d)
+
 
 func queue_narration(narration: String):
 	var n = NarrationBox.instance()
 	n.bbcode_text = narration
-	$margin/vbox.add_child(n)
+	self.queue_element(n)
+
+func queue_element(new_el: Control):
+	# Don't add anything more if there's already a queue
+	if not _postponed_elements.empty():
+		_postponed_elements.push_back(new_el)
+		return
+
+	# See if there is room for one more
+	if not self.present_element(new_el):
+		_postponed_elements.push_back(new_el)
+
+func present_element(new_el:Control)->bool:
+	# Total height of current elements
+	var content_height = 0
+	for el in $margin/vbox.get_children():
+		content_height += el.rect_size.y
+
+	# Height including the new element
+	var new_total_height = new_el.rect_size.y + content_height;
+
+	# Only add the new element if there is room for it
+	var height_limit = $margin/vbox.rect_size.y
+	if(new_total_height <= height_limit):
+		$margin/vbox.add_child(new_el)
+		return true
+	else:
+		return false
+
 
 func clear():
 	for c in $margin/vbox.get_children():
@@ -28,4 +59,20 @@ func clear():
 		c.queue_free()
 
 func _on_continue_button_pressed():
-	self.emit_signal("pressentation_completed")
+	# If there is nothing more to present
+	# then we are done here
+	if _postponed_elements.empty():
+		self.emit_signal("pressentation_completed")
+		return
+
+	# Make room for a new elements
+	self.clear()
+
+	# Attempt to add as many postponed elements as posible
+	while not _postponed_elements.empty():
+		var next_candidate = _postponed_elements.front()
+		if self.present_element(next_candidate):
+			_postponed_elements.pop_front()
+		else:
+			# Halt for now since there's no more room
+			return
