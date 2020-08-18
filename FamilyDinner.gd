dc61b2c667f9807d65029611d03f7ece944e3e20
Jakob Eklund <jakob.eklund@superattack.se>
2020-08-18 13:42:06 +0200

    Polish intro narrative (rephrasing and spelling)

--- FamilyDinner.gd
+++ FamilyDinner.gd
@@ -20,8 +20,9 @@ var _potential_conversations: Dictionary = {
 	# Real intro
 	"start": ConversationSegment.new("(Start the actuall story)", [
 	], [
-		{S: "mother-in-law", "text": "Welcome, dear!"},
-		{S: "mother-in-law", "text": "It brings such you to finaly get to meet you."},
+		{S: "mother-in-law", 
+		"text": "Welcome, dear!\n" +
+				"It's such a joy you to finally get to meet you."},
 	]),
 	"start->inlaws": ConversationSegment.new("It's great to finally meet my inlaws", [
 		{"follows": "start"},
@@ -51,19 +52,19 @@ var _potential_conversations: Dictionary = {
 		{S: "me", "text": "It seemd like the right thing to call you."},
 		{"text": "My father inlaw looks up from his newspaper."},
 		{S: "father-in-law", "text": "I was under the impression that you two met just recently."},
-		{"speaker": IV, "text": "You are?"},
+		{"speaker": IV, "text": "You where?"},
 		{"text": "I glance over at my BG/GF, "+
 			"who is currently occupied avoiding eyecontat with exactly every sentient being in the known universe."},
-		{"speaker": IV, "text": "What has he/sehe told them? Do I need to keep our sories traight for some good reason?"},
+		{"speaker": IV, "text": "What has he/she told them? Do I need to keep our stories traight for some reason?"},
 		{"text": "Suddenly. The kitchen timer rings."},
 		{S: "mother-in-law", "text": "Well, that's my queue. We'll have to talk more about this later."},
-		{"text": "She shifts gracefully in to the next room, wile keeping her eyes fixed on my fidgeting fiance."},
+		{"text": "She shifts gracefully in to the next room, while keeping her eyes fixed on my fidgeting fiance."},
 	]),
 	"start->home": ConversationSegment.new("I've been so looking forward to seeing my BFs/GFs home.", [
 		{"follows": "start"},
 	], [
-		{S: "me", "text": "I'm just so supprised by how lovely this home is!"},
-		{S: "sister-in-law", "text": "\"Supprised\"?"},
+		{S: "me", "text": "I'm just so suprised by how lovely this home is!"},
+		{S: "sister-in-law", "text": "\"Suprised\"?"},
 		{S: IV, "text": "\"Amazed\", I ment to say \"Amazed\"!"},
 		{S: "sister-in-law", "text": "Did you think we where poor? Living in some rough neighbourhood?"},
 		{S: SO, "text": "Sis! That's not what he/she said!"},
@@ -74,10 +75,11 @@ var _potential_conversations: Dictionary = {
 	], [
 		{"text": "My mother-in-law struggles to stay humble, whilst truly beaming with pride."},
 		{S: "mother-in-law", "text": "Well, it might be an old place."},
-		{S: "mother-in-law", "text": "Despit that I do think we've managed to creat a cozy home filled with love."},
+		{S: "mother-in-law", "text": "Despite everything I do think we've managed to creat a cozy home filled with love."},
 		{S: "father-in-law", "text": "Except when the boiler breaks or pipes jam."},
+		{"text": "He turns the page for dramatic effect."},
 		{S: "father-in-law", "text": "Then it's a cold home filled with bills."},
-		{S: "mother-in-law", "text": "Hmpf.. The house you wanted was.."},
+		{S: "mother-in-law", "text": "Hmpf.. The house you wanted on the other hand.."},
 		{"text": "Suddenly. The kitchen timer rings."},
 		{"text": "My mother-in-law marches out into the next room."},
 		{S: IV, "text": "Ooookeeey... Touchy subject?"},
@@ -85,12 +87,13 @@ var _potential_conversations: Dictionary = {
 	"start->home->modern": ConversationSegment.new("..it's so modern and practical", [
 		{"follows": "start->home"},
 	], [
-		{"text": "My father-in-law pops pup from behind his newspaper, "
-				+ "like someone called his name in a lotter raffel."},
-		{S: "father-in-law", "text": "Yes, why it is, isn't it!?"},
+		{S: "father-in-law", "text": "Yes, why yes it is, isn't it!?"},
+		{"text": "My father-in-law pops up from behind his newspaper,\n"
+				+ "like someone called his name in a lottery raffel."},
 		{S: "father-in-law", "text": "This house has all the latest modernities."},
 		{S: "mother-in-law", "text": "Yet it it only had a fireplace.."},
-		{S: "father-in-law", "text": "..which is expensive."},
+		{S: "father-in-law", "text": "..which is an expensive thing."},
+		{"text": "He dodges back behind his newspaper."},
 		{S: "father-in-law", "text": "It's also a fire hassard that.."},
 		{"text": "Suddenly. The kitchen timer rings."},
 		{"text": "My mother-in-law strides out into the next room."},
