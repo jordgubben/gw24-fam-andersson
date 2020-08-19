d7e79d556467253b4f576bf11d7f50c55a4d44b1
Jakob Eklund <jakob.eklund@superattack.se>
2020-08-19 08:21:55 +0200

    Refactor - Extract 'Conversation' node
    Move all general conversation behaviour into a separate node,
    so that it can be reused in multiple scenes.
    
    The example segments are now used as defaults only and no longer
    appear in the real storyline.

---
+++ Conversation.gd
@@ -0,0 +1,159 @@
+extends Node
+
+# Options panel
+const OptionsPanel = preload("res://ui/OptionsPanel.gd")
+export(NodePath) var options_path = NodePath("../ui/options")
+onready var _options_panel: OptionsPanel = self.get_node(options_path)
+
+# Narrative panel
+const NarrativePanel = preload("res://ui/NarrativePanel.gd")
+export(NodePath) var narrative_path = NodePath("../ui/narrative")
+onready var _narrative_panel: NarrativePanel = self.get_node(narrative_path)
+
+# Participants container
+export(NodePath) var participants_path = NodePath("../participants")
+onready var _participants: Node = self.get_node(participants_path)
+
+
+var _passed_segments = []
+var segments_db:Dictionary = {
+	"#1": ConversationSegment.new("First alternative from dictionary", [], [
+		{"speaker": "father-in-law", "text": "Dialog text from dictionary #1"},
+		{"text": "Narration dictionary #1"},
+	]),
+	"#1a": ConversationSegment.new("Another option (right after first)", [
+		{"follows": "#1"}
+	], [
+		{"text": "Follow up on #1"},
+	]),
+	"#2": ConversationSegment.new("Second alternative from dictionary", [], [
+		{"speaker": "mother-in-law", "text": "Dialog text from dictionary #2"},
+		{"text": "Narration dictionary #2"},
+	]),
+	"#2a": ConversationSegment.new("Different option (right after second)", [
+		{"follows": "#2"}
+	], [
+		{"text": "Follow up on #2"},
+	]),
+	"1+2=3": ConversationSegment.new("Conclusive option (after both 'originals')", [
+		{"passed": ["#1", "#2"]},
+	], [
+		{"text": "Follow up on #1 and #2"},
+	]),
+}
+
+func _ready():
+	self.populate_options()
+
+
+func populate_options():
+	_options_panel.clear_options()
+
+	# Filter out available conversations
+	var available_conversations = []
+	for key in segments_db.keys():
+		var segment:ConversationSegment = segments_db[key]
+		# Don't allow doing the same thing twice
+		if key in _passed_segments:
+			continue
+		# All prequisites must be fullfilled
+		if segment.is_presentable(_passed_segments):
+			available_conversations.append([key, segment])
+
+	# Add buttons for each available conversation
+	for a in available_conversations:
+		var key: String = a[0]
+		var segment:ConversationSegment = a[1]
+		var title: String = segment.title
+		_options_panel.add_option(key, title)
+
+func rewind_one_step():
+	_passed_segments.pop_back()
+	self.populate_options()
+
+func pressent_options():
+	_narrative_panel.visible = false
+	self.populate_options()
+	_options_panel.visible = true
+
+func present_segment(key:String):
+	# Hide options
+	_options_panel.visible = false
+
+	var speakers = {}
+	for c in _participants.get_children():
+		speakers[c.name] = c
+
+	# Present narrative pannel
+	_narrative_panel.clear();
+	var segment:ConversationSegment = segments_db[key]
+	segment.present(speakers, _narrative_panel)
+	_narrative_panel.visible = true
+
+	# Record as passed conversation
+	_passed_segments.push_back(key)
+
+#
+# A Narrative sequence is an exchange of thouhgts and feelings
+# between one or more characters.
+#
+class ConversationSegment:
+	var title: String = ""
+	var prequisites: Array = []
+	var narrative: Array = []
+
+	func _init(title:String, preq: Array, narr:Array):
+		self.title = title
+		self.prequisites = preq
+		self.narrative = narr
+
+	func is_presentable(passed: Array):
+		for pr in prequisites:
+			if not fullfilled(pr, passed):
+				return false
+		return true
+
+	func fullfilled(pr, passed: Array):
+		match pr:
+			# Require that this comes directly after the given statemen(s)
+			{"follows": var key}:
+				var last_segment_key = passed.back()
+				if key is String:
+					return last_segment_key ==  key
+				elif key is Array:
+					return last_segment_key in key
+				else:
+					printerr("Unrechognized type for 'follows':", typeof(key))
+
+			# Require that this comes sometime after the given statemen(s)
+			{"passed": var key}:
+				if key is String:
+					return key in passed
+				elif key is Array:
+					for k in key:
+						if not k in passed:
+							return false
+					return true
+				else:
+					printerr("Unrechognized type for 'passed':", typeof(key))
+
+			_:
+				printerr("Unrechognized prequisite:", pr)
+		#
+		return false
+
+	func present(speakers, out):
+		for el in self.narrative:
+			match el:
+				{"text": var text, "speaker": var speaker_id}:
+					if speaker_id in speakers:
+						var speaker = speakers[speaker_id]
+						out.queue_dialouge(speaker, text)
+					else:
+						printerr("Unknown speaker: ", speaker_id)
+
+				{"text": var text}:
+					out.queue_narration(text)
+
+				_:
+					printerr("Unablle to pressent", el)
--- FamilyDinner.gd
+++ FamilyDinner.gd
@@ -1,26 +1,15 @@
 extends Node2D
 
-# Options panel
-const OptionsPanel = preload("res://ui/OptionsPanel.gd")
-export(NodePath) var options_path = NodePath("ui/options")
-onready var _options_panel: OptionsPanel = self.get_node(options_path)
-
-# Narrative panel
-const NarrativePanel = preload("res://ui/NarrativePanel.gd")
-export(NodePath) var narrative_path = NodePath("ui/narrative")
-onready var _narrative_panel: NarrativePanel = self.get_node(narrative_path)
-
-var _passed_conversations = []
-
+const ConversationSegment = preload("res://Conversation.gd").ConversationSegment
 const S = "speaker"
 const SO = "significant_other"
 const IV = "inner_voice"
 
-var _potential_conversations: Dictionary = {
+var conversations_segments: Dictionary = {
 	# Real intro
 	"start": ConversationSegment.new("(Start the actuall story)", [
 	], [
-		{S: "mother-in-law", 
+		{S: "mother-in-law",
 		"text": "Welcome, dear!\n" +
 				"It's such a joy you to finally get to meet you."},
 	]),
@@ -110,33 +99,6 @@ var _potential_conversations: Dictionary = {
 		{S: "me", "text": "So.. *cough*"},
 		{S: "me", "text": "What nice weather we've had so far this summer."},
 	]),
-
-	# Separatpor + Dummy options (remove later)
-	"---": ConversationSegment.new("--- --- --- --- --- ---", [	], [	]),
-
-	"#1": ConversationSegment.new("First alternative from dictionary", [], [
-		{"speaker": "father-in-law", "text": "Dialog text from dictionary #1"},
-		{"text": "Narration dictionary #1"},
-	]),
-	"#1a": ConversationSegment.new("Another option (right after first)", [
-		{"follows": "#1"}
-	], [
-		{"text": "Follow up on #1"},
-	]),
-	"#2": ConversationSegment.new("Second alternative from dictionary", [], [
-		{"speaker": "mother-in-law", "text": "Dialog text from dictionary #2"},
-		{"text": "Narration dictionary #2"},
-	]),
-	"#2a": ConversationSegment.new("Different option (right after second)", [
-		{"follows": "#2"}
-	], [
-		{"text": "Follow up on #2"},
-	]),
-	"1+2=3": ConversationSegment.new("Conclusive option (after both 'originals')", [
-		{"passed": ["#1", "#2"]},
-	], [
-		{"text": "Follow up on #1 and #2"},
-	]),
 }
 
 
@@ -149,122 +111,16 @@ func _ready():
 	else:
 		$dining_room_occupants/significant_other.speaker_portrait = load("res://people/so_m_port.png")
 
-	self.populate_options()
-
-
-func populate_options():
-	_options_panel.clear_options()
-
-	# Filter out available conversations
-	var available_conversations = []
-	for key in _potential_conversations.keys():
-		var segment:ConversationSegment = _potential_conversations[key]
-		# Don't allow doing the same thing twice
-		if key in _passed_conversations:
-			continue
-		# All prequisites must be fullfilled
-		if segment.is_presentable(_passed_conversations):
-			available_conversations.append([key, segment])
-
-	# Add buttons for each available conversation
-	for a in available_conversations:
-		var key: String = a[0]
-		var segment:ConversationSegment = a[1]
-		var title: String = segment.title
-		_options_panel.add_option(key, title)
-
+	# Prep. conversation
+	$conversation.segments_db = conversations_segments
+	$conversation.populate_options()
 
 func _on_options_option_selected(key):
-	# Hide options
-	_options_panel.visible = false
-
-	var speakers = {}
-	for c in $dining_room_occupants.get_children():
-		speakers[c.name] = c
-
-	# Present narrative pannel
-	_narrative_panel.clear();
-	var segment:ConversationSegment = _potential_conversations[key]
-	segment.present(speakers, _narrative_panel)
-	_narrative_panel.visible = true
-
-	# Record as passed conversation
-	_passed_conversations.push_back(key)
-
-
+	$conversation.present_segment(key)
 
 func _on_narrative_pressentation_completed():
-	_narrative_panel.visible = false
-	self.populate_options()
-	_options_panel.visible = true
-
-#
-# A Narrative sequence is an exchange of thouhgts and feelings
-# between one or more characters.
-#
-class ConversationSegment:
-	var title: String = ""
-	var prequisites: Array = []
-	var narrative: Array = []
-
-	func _init(title:String, preq: Array, narr:Array):
-		self.title = title
-		self.prequisites = preq
-		self.narrative = narr
-
-	func is_presentable(passed: Array):
-		for pr in prequisites:
-			if not fullfilled(pr, passed):
-				return false
-		return true
-
-	func fullfilled(pr, passed: Array):
-		match pr:
-			# Require that this comes directly after the given statemen(s)
-			{"follows": var key}:
-				var last_segment_key = passed.back()
-				if key is String:
-					return last_segment_key ==  key
-				elif key is Array:
-					return last_segment_key in key
-				else:
-					printerr("Unrechognized type for 'follows':", typeof(key))
-
-			# Require that this comes sometime after the given statemen(s)
-			{"passed": var key}:
-				if key is String:
-					return key in passed
-				elif key is Array:
-					for k in key:
-						if not k in passed:
-							return false
-					return true
-				else:
-					printerr("Unrechognized type for 'passed':", typeof(key))
-
-			_:
-				printerr("Unrechognized prequisite:", pr)
-		#
-		return false
-
-	func present(speakers, out):
-		for el in self.narrative:
-			match el:
-				{"text": var text, "speaker": var speaker_id}:
-					if speaker_id in speakers:
-						var speaker = speakers[speaker_id]
-						out.queue_dialouge(speaker, text)
-					else:
-						printerr("Unknown speaker: ", speaker_id)
-
-				{"text": var text}:
-					out.queue_narration(text)
-
-				_:
-					printerr("Unablle to pressent", el)
-
+	$conversation.pressent_options()
 
 func _on_rewind_btn_pressed():
 	prints("<< Rewinding a by a single step")
-	_passed_conversations.pop_back()
-	self.populate_options()
+	$conversation.rewind_one_step()
